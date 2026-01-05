ddd6cc295efc72efff7d3c444e5755590d66c582
User <b.mwololo@alustudent.com>
2026-01-05 19:48:10 +0400

    I added function to read data from file to load saved data

--- file_manager.py
+++ file_manager.py
@@ -24,3 +24,17 @@ class FileManager:
         except IOError as e:
             print(f"Error writing to file: {e}")
             return False
+
+    def read_from_file(self):
+        try:
+            with open(self.fileName, 'r') as file:
+                lines = file.readlines()
+            return [line.strip() for line in lines if line.strip()]
+
+        except FileNotFoundError:
+
+            print(f"File {self.fileName} not found")
+            return []
+        except IOError as e:
+            print(f"Error reading file: {e}")
+            return []
