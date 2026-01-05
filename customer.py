426a1ac122e7098cac3e6639628c05c4fb46b501
User <b.mwololo@alustudent.com>
2026-01-05 20:06:59 +0400

    Customer class and basic customer details

---
+++ customer.py
@@ -0,0 +1,9 @@
+
+class Customer:
+    def __init__(self, customer_id, name, contact_details, license_number):
+
+        # Basic customer information
+        self.customerID = customer_id
+        self.name = name
+        self.contactDetails = contact_details
+        self.licenseNumber = license_number
