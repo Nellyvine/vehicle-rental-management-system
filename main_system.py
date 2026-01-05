e3e71a942167ebe1341228aa132867e8883d09fa
Nellyvine <n.mizero@alustudent.com>
2026-01-05 17:10:06 +0400

    added cleat screen and pause function to keep the output flow clean

--- main_system.py
+++ main_system.py
@@ -9,7 +9,7 @@ class MainSystem:
         self.payment_file = FileManager("payments.txt")
         self.user_file = FileManager("users.txt")
 
-# Initialize files with headers
+# Initialize files with headers if they are empty
         self._initialize_file_headers()
 
     def _initialize_file_headers(self):
@@ -22,3 +22,18 @@ class MainSystem:
             "payments.txt": "PaymentID|RentalID|AmountPaid|PaymentDate",
             "users.txt": "Username|Password"
         }
+
+        for filename, header in files_and_headers.items():
+# Only add header if file is empty
+            if os.path.exists(filename) and os.path.getsize(filename) == 0:
+                with open(filename, 'w') as f:
+                    f.write(header + '\n')
+
+    def clear_screen(self):
+# Clears the console screen to keep the output clean
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def pause(self):
+# Waits for user to press Enter before continuing
+        input("\nPress Enter to continue...")
+
